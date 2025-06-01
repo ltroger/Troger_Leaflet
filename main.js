85c36e0eff4ebb1ece3ce73c048a75abc270cff6
fhs50561 <ltroger.mmt-b2023@fh-salzburg.ac.at>
2025-06-01 18:55:18 +0200

    leaflet

---
+++ main.js
@@ -0,0 +1,115 @@
+const map = L.map('map').setView([47.8095, 13.0550], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var circle = L.circle([47.8095, 13.0550], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+});
+
+circle.bindPopup("Gaming.");
+
+const chargingQuery = `
+[out:json];
+area[name="Salzburg"]->.searchArea;
+node["amenity"="charging_station"](area.searchArea);
+out center;
+`;
+
+fetch('https://overpass-api.de/api/interpreter', {
+    method: 'POST',
+    body: chargingQuery,
+})
+    .then(res => res.json())
+    .then(data => {
+        data.elements.forEach(el => {
+            const latitude = el.lat;
+            const longitude = el.lon;
+            const tags = el.tags || {};
+
+            const name = tags.name || 'Charging Station';
+            const operator = tags.operator || 'Unknown operator';
+            const opening_hours = tags.opening_hours || 'N/A';
+            const socket = tags.socket_type || tags.socket || 'N/A';
+
+            const marker = L.circle([latitude, longitude], {
+                color: 'blue',
+                fillColor: '#30f',
+                fillOpacity: 0.5,
+                radius: 100
+            }).addTo(map);
+
+            marker.bindTooltip(name);
+            marker.bindPopup(`
+                <strong>${name}</strong><br>
+                Operator: ${operator}<br>
+                Opening hours: ${opening_hours}<br>
+                Socket type: ${socket}
+            `);
+        });
+    });
+
+
+
+const fuelQuery = `
+[out:json];
+area[name="Salzburg"]->.searchArea;
+node["amenity"="fuel"](area.searchArea);
+out center;
+`;
+
+fetch('https://overpass-api.de/api/interpreter', {
+    method: 'POST',
+    body: fuelQuery,
+})
+    .then(res => res.json())
+    .then(data => {
+        data.elements.forEach(el => {
+            const latitude = el.lat;
+            const longitude = el.lon;
+            const tags = el.tags || {};
+
+            const name = tags.name || 'Fuel Station';
+            const operator = tags.operator || 'Unknown operator';
+            const opening_hours = tags.opening_hours || 'N/A';
+            const brand = tags.brand || 'Unknown brand';
+            const fuel_diesel = tags['fuel:diesel'] ? 'Yes' : 'No';
+            const fuel_gasoline = tags['fuel:gasoline'] ? 'Yes' : 'No';
+
+            const marker = L.circle([latitude, longitude], {
+                color: 'green',
+                fillColor: '#3f3',
+                fillOpacity: 0.5,
+                radius: 100
+            }).addTo(map);
+
+            marker.bindTooltip(name);
+            marker.bindPopup(`
+                <strong>${name}</strong><br>
+                Operator: ${operator}<br>
+                Opening hours: ${opening_hours}</br>
+                Brand: ${brand}<br>
+                Diesel: ${fuel_diesel}<br>
+                Gasoline: ${fuel_gasoline}
+            `);
+        });
+    });
+
+const legend = L.control({ position: 'bottomleft' });
+
+legend.onAdd = function () {
+    const div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = `
+    <h4>Legend</h4>
+    <i style="background:blue; width: 20px; height: 20px; display: inline-block;"></i><b> Charging stations</b><br>
+    <i style="background:green; width: 20px; height: 20px; display: inline-block;"></i><b> Fuel stations</b><br>
+  `;
+    return div;
+};
+
+legend.addTo(map);
